3a39aafe0b6c2c2db964aeec123e4e6d6b55a56b
Khalil Claybon <khalil.claybon@yodle.com>
2017-12-10 04:19:14 -0500

    add server listening

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
+	"net/http"
 
-	"github.com/google/go-github/github"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	file, e := ioutil.ReadFile("./example.json")
-	if e != nil {
-		fmt.Printf("File error: %v\n", e)
-		os.Exit(1)
+	router := NewRouter()
+	log.Println("Webhook server listening on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", &MyServer{router}))
+}
+
+//MyServer . . . type is a struct containing a pointer to a mux.router object
+type MyServer struct {
+	r *mux.Router
+}
+
+func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods", "GET")
 	}
-	evt := github.ReleaseEvent{}
-	if err := json.Unmarshal(file, &evt); err != nil {
-		log.Printf("Failed processing hook! ('%s')", err)
+
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
 	}
-	err := UploadArtifacts(evt)
-	log.Fatal(err)
+	// Lets Gorilla work
+	s.r.ServeHTTP(rw, req)
 }
